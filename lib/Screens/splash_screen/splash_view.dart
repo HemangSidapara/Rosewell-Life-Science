837b2e2363af994acad96b8eeee9335345e4d991
Hemang Sidapara <hemang@logisticinfotech.co.in>
2024-01-12 20:06:00 +0530

    splash changes

--- lib/Screens/splash_screen/splash_view.dart
+++ lib/Screens/splash_screen/splash_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:rosewell_life_science/Constants/app_assets.dart';
 import 'package:rosewell_life_science/Constants/app_colors.dart';
-import 'package:rosewell_life_science/Constants/app_strings.dart';
 import 'package:rosewell_life_science/Screens/splash_screen/splash_controller.dart';
 import 'package:rosewell_life_science/Utils/app_sizer.dart';
 
@@ -30,24 +29,9 @@ class _SplashViewState extends State<SplashView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              AppAssets.splashImage,
-              width: context.isPortrait ? 60.w : 30.w,
-            ),
-            SizedBox(height: 3.h),
-            Text(
-              AppStrings.appName,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: AppColors.PRIMARY_COLOR,
-                fontSize: context.isPortrait ? 23.sp : 18.sp,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-          ],
+        child: Image.asset(
+          AppAssets.splashImage,
+          width: context.isPortrait ? 80.w : 50.w,
         ),
       ),
     );
